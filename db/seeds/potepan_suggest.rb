07b69dc9e6cff1eefe2bd6530d9eb23a50353393
furugen <furugen@MacBook-Pro-fur.local>
2020-01-20 17:36:37 +0900

    課題５用設定追加

---
+++ db/seeds/potepan_suggest.rb
@@ -0,0 +1,25 @@
+[
+  'apache',
+  'apache for women',
+  'apache for men',
+  'bag',
+  'bag for women',
+  'bag for men',
+  'mug',
+  'shirt',
+  'shirt for women',
+  'shirt for men',
+  't-shirt',
+  'ruby',
+  'ruby for women',
+  'ruby for men',
+  'rails',
+  'rails for women',
+  'rails for men',
+  'RUBY ON RAILS',
+  'RUBY ON RAILS bag',
+  'RUBY ON RAILS t-shirt',
+  'TOTE',
+].each do |keyword|
+  Potepan::Suggest.create!(keyword: keyword)
+end
