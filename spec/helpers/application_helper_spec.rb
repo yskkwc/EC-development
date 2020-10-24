7d03a36a04352d798a89684716d8b09b3570442c
yskkwc <y.k.trial.serv@gmail.com>
2020-10-24 23:41:20 +0900

    Add application_helper_spec.rb & Include application_helper.rb_to_rails_helper.rb

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "page_title" do
+    let(:page_title) { "Test_page" }
+
+    context "have indivisual title" do
+      it "display title 'Test_page - BIGBAG Store'" do
+        expect(full_title(page_title: page_title)).to eq "#{page_title} - #{BASE_TITLE}"
+      end
+    end
+
+    context "not_have indivisual title" do
+      it "display title 'BIGBAG Store' when title blank" do
+        expect(full_title(page_title: "")).to eq "#{BASE_TITLE}"
+      end
+      it "display title 'BIGBAG Store' when title nil" do
+        expect(full_title(page_title: nil)).to eq "#{BASE_TITLE}"
+      end
+    end
+  end
+end
