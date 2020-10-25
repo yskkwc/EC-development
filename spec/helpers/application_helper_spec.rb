0445249ea3ee090bdb9d4f62384d137db0a39eb2
yskkwc <y.k.trial.serv@gmail.com>
2020-10-25 18:16:52 +0900

    Change rails_helper.rb_with_application_helper.rb_for_fail -compose up

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe ApplicationHelper, type: :helper do
 
     context "have indivisual title" do
       it "display title 'Test_page - BIGBAG Store'" do
-        expect(full_title(page_title: page_title)).to eq "#{page_title} - #{BASE_TITLE}"
+        expect(full_title(page_title: page_title)).to eq "Test_page - BIGBAG Store"
       end
     end
 
     context "not_have indivisual title" do
       it "display title 'BIGBAG Store' when page_title: ''" do
-        expect(full_title(page_title: "")).to eq "#{BASE_TITLE}"
+        expect(full_title(page_title: "")).to eq "BIGBAG Store"
       end
       it "display title 'BIGBAG Store' when page_title: nil" do
-        expect(full_title(page_title: nil)).to eq "#{BASE_TITLE}"
+        expect(full_title(page_title: nil)).to eq "BIGBAG Store"
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-include ApplicationHelper
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
