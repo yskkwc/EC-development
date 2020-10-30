7db1017ecc27367c46326d7d520abb2883351e88
yskkwc <y.k.trial.serv@gmail.com>
2020-10-31 01:22:42 +0900

    コメント編集箇所:環境変数の設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,12 +95,12 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     preserve_files: true,
-    s3_host_name: 's3-ap-northeast-1.amazonaws.com',
+    s3_host_name: ENV['AWS_HOST_NAME'],
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: 'ap-northeast-1'
+      s3_region: ENV['AWS_REGION']
     },
-    bucket: 'potepanec-main-task-yskkwc'
+    bucket: ENV['AWS_BUCKET_NAME']
   }
 end
